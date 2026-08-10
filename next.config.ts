e44bd2b9877697144678f12da8059b9b65cddfa7
FellypeGC <gabrielfellypesteve@gmail.com>
2026-08-10 20:40:42 -0300

    chore: add turbopack

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    turbopackPluginRuntimeStrategy: "workerThreads",
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
